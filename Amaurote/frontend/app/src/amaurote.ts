1e077d1b7276bb5edebf168abc4ca9ad9c5808c9
Серега Батагов <ironrate@yandex.ru>
2018-12-10 00:28:00 +0300

    modify

--- Amaurote/frontend/app/src/amaurote.ts
+++ Amaurote/frontend/app/src/amaurote.ts
@@ -1,8 +1,6 @@
 import THREE = require("three");
 import FirstPersonControls from "first-person-controls";
 
-
-
 export class Amaurote {
   private static enclosureThis: Amaurote;
 
@@ -31,25 +29,34 @@ export class Amaurote {
     this.camera.position.z = 1;
 
     this.scene = new THREE.Scene();
+    this.clock = new THREE.Clock(true);
 
-    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    this.material = new THREE.MeshNormalMaterial();
-
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(this.mesh);
+   
 
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     document.body.appendChild(this.renderer.domElement);
     window.addEventListener("resize", this.onResize);
     this.onResize();
-    this.initUserControl();
-    this.clock = new THREE.Clock(true);
+    //this.initUserControl();
+    this.initScene();
   }
 
   private initUserControl() {
     this.controls = new FirstPersonControls(this.camera);
     this.controls.movementSpeed = 1;
     this.controls.lookSpeed = 0.1;
+  }
+
+  private initScene() {
+    const helper = new THREE.GridHelper(200, 10, 0x0000ff, 0x808080);
+    helper.position.y = -10;
+    this.scene.add(helper);
+
+    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    this.material = new THREE.MeshNormalMaterial();
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
   }
 
   private onResize() {
@@ -68,8 +75,10 @@ export class Amaurote {
     self.mesh.rotation.x += 0.01;
     self.mesh.rotation.y += 0.02;
 
-    self.controls.update(self.clock.getDelta());
     self.renderer.render(self.scene, self.camera);
+    if (self.controls) {
+      self.controls.update(self.clock.getDelta());
+    }
   }
 }
 
